3c4bf557106988c5c3d2c5c32b8d5df849452dec
BrunoHiramuki <hiramuki15@gmail.com>
2025-07-02 21:03:28 -0300

    Adicionar arquivos de projeto.

---
+++ Treino1/Controllers/ProdutoController.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Treino1.Model;
+using Treino1.Repositorios;
+
+namespace Treino1.Controllers
+{
+    public class ProdutoController : ControllerBase
+    {
+        [HttpGet("GetProdutos")]
+        public ActionResult<List<Produto>> Get()
+        {
+            List<Produto> produtos = ProdutosApi.ObterTodos();
+            return Ok(produtos);
+        }
+
+        [HttpGet("GetProduto")]
+        public ActionResult<Produto> Get([FromQuery] int id)
+        {
+            Produto produto = ProdutosApi.ObterPorId(id);
+            return produto;
+        }
+
+        [HttpPost("AddProduto")]
+        public ActionResult<Produto> Add([FromBody] Produto produto)
+        {
+            if (produto == null)
+            {
+                return BadRequest("Produto não pode ser nulo.");
+            }
+
+            ProdutosApi.Adicionar(produto);
+
+            return Ok(produto);
+        }
+
+        [HttpPut("AtualizarProduto")]
+        public ActionResult<Produto> Put([FromBody] Produto produto)
+        {
+            if (produto == null)
+            {
+                BadRequest("Preencha os dados do produto");
+            }
+
+            ProdutosApi.Atualizar(produto);
+            return Ok(produto);
+        }
+
+        [HttpDelete("DeletarProduto")]
+        public ActionResult Delete ([FromQuery] int id)
+        {
+            ProdutosApi.Remover(id);
+            return Ok();
+        }
+
+
+    }
+}
---
+++ Treino1/Repositorios/ProdutosApi.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Treino1.Model;
+
+namespace Treino1.Repositorios;
+
+public class ProdutosApi
+{
+    private static List<Produto> produtos = new List<Produto>();
+    private static int produtoID = 1;
+
+    public static List<Produto> ObterTodos()
+    {
+        return produtos;
+    } 
+
+    public static Produto ObterPorId (int id)
+    {
+        return produtos.FirstOrDefault(p => p.Id == id);
+    }
+
+    public static Produto Adicionar(Produto produto)
+    {
+        produto.Id = produtoID++;
+        produtos.Add(produto);
+        
+        return produto;
+    }
+
+    public static Produto Atualizar(Produto produto)
+    {
+        int index = produtos.FindIndex(p=> p.Id == produto.Id);
+        if (index != -1)
+            produtos[index] = produto;
+
+        return produto;
+    }
+
+    public static void Remover (int id)
+    {
+        Produto produto = ObterPorId(id);
+
+        produtos.Remove(produto);
+    }
+
+}
